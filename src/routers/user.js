374db3be10817cb3de0901114e31f85932d970ea
PemulungEmperor <yuliuslebelamak@yahoo.com>
2023-07-05 03:31:44 +0700

    multer response size

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 
 const jwt = require("jsonwebtoken");
 const router = express.Router();
@@ -13,7 +14,29 @@ router.get("/user", getUserFromToken);
 router.get("/users/:id", getById);
 
 // update image
-router.put("/update/profile-img/:id", multerUpload, updateProfileImage);
+router.put(
+  "/update/profile-img/:id",
+  (req, res) => {
+    multerUpload(req, res, (err) => {
+      if (err) {
+        if (err instanceof multer.MulterError) {
+          if (err.code == "LIMIT_FILE_SIZE")
+            res.status(400).send({
+              code: "large",
+              message: "File is large ,only upload files less than 2MB",
+            });
+          else {
+            res.status(400).send({
+              code: "unknown",
+              message: "Error while uploading",
+            });
+          }
+        }
+      }
+    });
+  },
+  updateProfileImage
+);
 
 // update user
 router.put("/update/profile/:id", updateProfile);
